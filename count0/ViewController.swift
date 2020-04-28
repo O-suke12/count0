65fd1d0633869a437ac840493e2b6f55db8613a6
O-suke12 <souke0504@gmail.com>
2020-04-28 17:48:07 +0900

    2nd commit

--- count0/ViewController.swift
+++ count0/ViewController.swift
@@ -14,7 +14,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    @IBAction func plus() {
+        number = number+1
+        label.text = String(number)
+        if number >= 10{
+           label.textColor = UIColor.black
+           } else {
+           label.textColor = UIColor.white
+           }
+    }
+    @IBAction func minus() {
+        number = number-1
+        label.text = String(number)
+        
+        }
+    @IBAction func kakeru() {
+        number = number*100
+        label.text = String(number)
+        
+    }
+    @IBAction func waru() {
+        number = number/number
+        label.text = String(number)
+    }
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+    }
+    
 }
 
