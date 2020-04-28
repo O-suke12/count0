f09069119527f3f5d533597a56eaf7dd8e125ab1
RS <RS@RS-no-MacBook-Pro.local>
2020-04-28 17:44:41 +0900

    1st commit

---
+++ count0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  count0
+//
+//  Created by RS on 2020/04/27.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
